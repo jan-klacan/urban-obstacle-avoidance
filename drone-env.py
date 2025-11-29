e8a67cd8fd3f4630c74094ba2f5e9196c27e6573
Jan Klacan <156678842+jan-klacan@users.noreply.github.com>
2025-11-29 21:04:53 +0100

    Add self.observation_space, improve self.action_space

--- drone-env.py
+++ drone-env.py
@@ -4,10 +4,23 @@ import gymnasium as gym
 from gymnasium import spaces
 
 class DroneEnv(gym.Env):
-    def __init__self(self):
+    def __init__(self):
         super(DroneEnv, self).__init__()
 
         # ACTION SPACE - WHAT THE DRONE CAN DO
-        # - use spaces.Box to represent a continuous velocity vector
-        # - - (a 2D vector where each component is a real value between -1 and 1)
+        # - use spaces.Box to represent a continous velocity vector (2D)
+        # - each component a real value between -1 and 1
         self.action_space = spaces.Box(low=-1, high=1, shape=(2,), dtype=np.float32)
+
+        # OBSERVATION SPACE - WHAT THE DRONE CAN SEE
+        # - use spaces.Box to represent a 12D vector
+        # - - drone position: 2 numbers
+        # - - target position: 2 numbers
+        # - - LIDAR readings: 8 numbers
+        # - all values are normalized to [0, 1]
+        self.observation_space = spaces.Box(
+            low=0,
+            high=1,
+            shape= (12,),
+            dtype=np.float32
+        )
